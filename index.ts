8b74ceedb08755e4ae8d5df31a088d9f47084bd8
IamFlowZ <dakota.lewallen17@gmail.com>
2022-10-23 12:06:26 -0400

    comment with output

--- index.ts
+++ index.ts
@@ -8,3 +8,54 @@ import withoutClient from './lib/without-client'
   console.log(`withoutClientResult: ${JSON.stringify(withoutClientResult, null, 2)}`);
 })()
 console.log('hello');
+
+/**
+ * withClientResult: {
+    "Items": [
+      {
+        "finishPos": 20,
+        "sk": "spa#2015",
+        "pk": "driver#leclerc"
+      },
+      {
+        "finishPos": 6,
+        "sk": "spa#2022",
+        "dnf": false,
+        "pk": "driver#leclerc"
+      }
+    ],
+    "Count": 2,
+    "ScannedCount": 2
+  }
+  withoutClientResult: {
+    "Items": [
+      {
+        "finishPos": {
+          "N": "20"
+        },
+        "sk": {
+          "S": "spa#2015"
+        },
+        "pk": {
+          "S": "driver#leclerc"
+        }
+      },
+      {
+        "finishPos": {
+          "N": "6"
+        },
+        "sk": {
+          "S": "spa#2022"
+        },
+        "dnf": {
+          "BOOL": false
+        },
+        "pk": {
+          "S": "driver#leclerc"
+        }
+      }
+    ],
+    "Count": 2,
+    "ScannedCount": 2
+  }
+ */
